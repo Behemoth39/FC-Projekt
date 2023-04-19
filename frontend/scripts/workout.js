4117d276acde7dcac637913240b7ebcf6ceb4077
Behemoth39 <simme_jonsson@hotmail.com>
2023-04-19 14:21:17 +0200

    redid workout page

--- frontend/scripts/workout.js
+++ frontend/scripts/workout.js
@@ -1,5 +1,4 @@
-const workout = document.querySelector("#workout-tables");
-/* lyckas inte att få detta å fungera */
+const workout = document.querySelector("#workouts");
 
 const listWorkouts = async () => {
   const url = "http://localhost:3000/workouts";
@@ -8,12 +7,38 @@ const listWorkouts = async () => {
 
   if (response.status == 200) {
     const data = await response.json();
-    console.log(data);
 
     data.forEach((exercises) => {
-      /* Bara för test */
-      workout.insertAdjacentHTML("beforeend", `<p>${exercises.exercise}</p>`);
+      workout.insertAdjacentHTML(
+        "beforeend",
+        ` <div class="container">
+          <div class="head">
+            <h3>${exercises.exercise}</h3>
+            <p class="edit">E</p>
+          </div>
+          <div class="content">
+            <p>${exercises.sets}</p>
+            <p>${exercises.reps}</p>
+            <p>${exercises.weight}</p>
+            <p class="edit">E</p>
+          </div>
+          <div class="add-set">Add set</div>
+        </div>
+        <div class="buttons">
+          <button>Add exercise</button>
+          <button>Logs</button>
+        </div>`
+      );
     });
+
+    /*for (let i = 0; i < exercise.sets; i++) {
+      <div class='content'>
+        <p>${i++}</p>
+        <p>${exercises.reps}</p>
+        <p>${exercises.weight}</p>
+        <p class='edit'>E</p>
+      </div>;
+    }*/
   }
 };
 
