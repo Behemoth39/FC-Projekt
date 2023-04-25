05c992fadc0fddca8d9728dc989ab80ce1458b3c
Behemoth39 <simme_jonsson@hotmail.com>
2023-04-25 13:54:04 +0200

    PANIC!!!

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,90 +1,162 @@
-const app = {
-  login: document.getElementById("log-in"),
-  logout: document.getElementById("log-out"),
-  username: document.getElementById("user"),
-  password: document.getElementById("pass"),
-  againBtn: document.getElementById("again-btn"),
-  loginBtn: document.getElementById("login-btn"),
-  loginForm: document.getElementById("login-form"),
-  loginMenu: document.getElementById("login-menu"),
-  loginH2: document.getElementById("log-h2"),
-  userLogged: localStorage.getItem("userLogged"),
-  errorMsgUser: document.getElementById("error-msg-login"),
-  nav: document.querySelector(".navbar-screen"),
-};
-
-const loginForm = () => {  
+/*
+const loginForm = () => {
   app.nav.insertAdjacentHTML(
     "afterend",
-    ` <div class="loginContainer hidden" id="login-menu">
-        <div id="closebtn">X</div>
-        <form class="login-windows" id="login-form">
-          <p id="error-msg-login" class="hidden">Felaktigt användarnamn/lösenord!</p>
-          <h2 id="log-h2">LOGIN</h2>
-          <input id="user" class="login-window" placeholder="namn" type="text" />
-          <input id="pass" class="login-window" placeholder="lösenord" type="password" />
-          <div class="btn-row">
-            <div>
-              <input type="button" value="LOGGA IN" id="login-btn" class="btn" />
-            </div>
-            <div>
-              <input type="button" value="FÖRSÖK IGEN" id="again-btn" class="btn hidden" />
-            </div>
-          </div>
-        </form>
-      </div>`
+    `<div class="loginContainer">
+    <div id="closebtn">X</div>
+    <h1 id="h1-sign-in">Sign In</h1>
+    <h1 id="h1-sign-up">Sign Up</h1>
+    <form id="login-form" class="login-windows">
+      <p id="error-msg-login" class="error-msg">Invalid Username/Password!</p>
+      <p id="error-msg-create" class="error-msg">
+        Username and password needs to be at least 5 characters in lenght!
+      </p>
+      <input
+        id="user"
+        class="login-window"
+        name="array[]"
+        placeholder="User Name"
+        type="text"
+      />
+      <input
+        id="pass"
+        class="login-window"
+        name="array[]"
+        placeholder="Password"
+        type="password"
+      />
+      <div class="btn-row">
+        <div>
+          <input type="button" value="SIGN IN" id="login-btn-in" class="btn" />
+        </div>
+        <div>
+          <input type="button" value="SIGN UP" id="login-btn-up" class="btn" />
+        </div>
+        <div>
+          <input
+            type="button"
+            value="Create Account"
+            id="create-btn"
+            class="btn"
+          />
+        </div>
+      </div>
+    </form>`
   );
+};*/
+
+const app = {
+  loginForm: document.getElementById("login-form"),
+  loginMenu: document.getElementById("login-menu"),
+  login: document.getElementById("log-in"),
+  logout: document.getElementById("log-out"),
+  signIn: document.getElementById("login-btn-in"),
+  signUpBtn: document.getElementById("login-btn-up"),
+  h1SignIn: document.getElementById("h1-sign-in"),
+  h1SignUp: document.getElementById("h1-sign-up"),
+  createBtn: document.getElementById("create-btn"),
+  errorMsgUser: document.getElementById("error-msg-login"),
+  errorMsgCreate: document.getElementById("error-msg-create"),
+  userLogged: localStorage.getItem("userLogged"),
+  savedUsername: [],
+  savedPassword: [],
 };
 
- loginForm()
-
-function clearForm() {
+function errorMsg() {
+  app.errorMsgUser.classList.remove("error-msg");
+  setTimeout(function () {
+    app.errorMsgUser.classList.add("error-msg");
+  }, 2000);
   app.loginForm.user.value = null;
   app.loginForm.pass.value = null;
 }
 
-function errorMsg() {
-  app.loginBtn.classList.add("hidden");
-  app.username.classList.add("hidden");
-  app.password.classList.add("hidden");
-  app.againBtn.classList.add("hidden");
-  app.loginH2.classList.add("hidden");
-  app.againBtn.classList.remove("hidden");
-  app.errorMsgUser.classList.remove("hidden");
+function removeSignInUP() {
+  app.signIn.style.display = "none";
+  app.signUpBtn.style.display = "none";
+  app.h1SignIn.style.display = "none";
+  app.h1SignUp.style.display = "block";
+  app.createBtn.style.display = "block";
+  app.errorMsgUser.classList.add("error-msg");
 }
 
-function removeErrorMsg() {
-  app.loginBtn.classList.remove("hidden");
-  app.username.classList.remove("hidden");
-  app.password.classList.remove("hidden");
-  app.againBtn.classList.remove("hidden");
-  app.loginH2.classList.remove("hidden");
-  app.againBtn.classList.add("hidden");
-  app.errorMsgUser.classList.add("hidden");
+function addSignInUP() {
+  app.signIn.style.display = "inline-block";
+  app.signUpBtn.style.display = "inline-block";
+  app.createBtn.style.display = "none";
+  app.h1SignIn.style.display = "block";
+  app.h1SignUp.style.display = "none";
+  app.errorMsgCreate.classList.add("error-msg");
+  app.loginMenu.style.display = "flex";
 }
 
-function removeLoginMenu() {
-  app.loginMenu.classList.add("hidden");
-  app.login.classList.add("hidden");
-  app.logout.classList.remove("hidden");
-}
-
-function addLoginMenu() {
-  app.login.classList.remove("hidden");
-  app.logout.classList.add("hidden");
-}
-
-document.getElementById("log-in").addEventListener("click", (e) => {
+app.login.addEventListener("click", (e) => {
   e.preventDefault;
-  app.loginMenu.classList.remove("hidden");
+  app.loginMenu.style.display = "flex";
 });
 
 document.getElementById("closebtn").addEventListener("click", (e) => {
   e.preventDefault;
-  app.loginMenu.classList.add("hidden");
+  app.loginMenu.style.display = "none";
 });
 
-document.getElementById("login-btn").addEventListener("click", (e) => {
+document.getElementById("login-btn-in").addEventListener("click", (e) => {
+  e.preventDefault;
+  username = app.loginForm.user.value;
+  password = app.loginForm.pass.value;
+
+  for (let i = 0; i < app.savedUsername.length; i++) {
+    if (username == app.savedUsername[i] && password == app.savedPassword[i]) {
+      app.loginMenu.style.display = "none";
+      app.login.style.display = "none";
+      app.logout.style.display = "block";
+    } else {
+      errorMsg();
+    }
+  }
+});
+
+document.getElementById("login-btn-up").addEventListener("click", (e) => {
+  e.preventDefault;
+  removeSignInUP();
+});
+
+function adduser() {
+  // <= remove when done, used to see saved password and usernamne
+  let saved1 = app.savedUsername.map((user) => `<li>${user}</li>`).join("\n");
+  document.querySelector("#ulus").innerHTML = saved1;
+  let saved2 = app.savedPassword.map((user) => `<li>${user}</li>`).join("\n");
+  document.querySelector("#ulpa").innerHTML = saved2;
+}
+
+document.getElementById("create-btn").addEventListener("click", (e) => {
+  e.preventDefault;
+  if (app.loginForm.user.value.length < 5 || app.loginForm.pass.value.length < 5) {
+    app.errorMsgCreate.classList.remove("error-msg");
+    setTimeout(function () {
+      app.errorMsgCreate.classList.add("error-msg");
+    }, 4000);
+  } else {
+    app.savedUsername.push(app.loginForm.user.value);
+    app.savedPassword.push(app.loginForm.pass.value);
+    app.loginForm.user.value = null;
+    app.loginForm.pass.value = null;
+    adduser(); // <= remove when done
+    addSignInUP();
+  }
+});
+
+/* save locally instead
+  
+ localStorage.setItem('username'); //save in array 
+ localStorage.setItem('password'); //save in array 
+let saveduser =  localStorage.setItem('username');
+let savedpass =  localStorage.setItem('password');
+localStorage.removeItem('password');
+localStorage.clear;  
+ */
+
+/*document.getElementById("login-btn").addEventListener("click", (e) => {
   e.preventDefault;
   let username = app.loginForm.user.value;
   let password = app.loginForm.pass.value;
@@ -97,21 +169,17 @@ document.getElementById("login-btn").addEventListener("click", (e) => {
   }
 });
 
+
+to keep users logged in
 if (app.userLogged) {
   removeLoginMenu();
 } else {
   addLoginMenu();
-}
 
-document.getElementById("again-btn").addEventListener("click", (e) => {
-  e.preventDefault;
-  removeErrorMsg();
-  clearForm();
-});
-
-document.getElementById("log-out").addEventListener("click", (e) => {
+  document.getElementById("log-out").addEventListener("click", (e) => {
   e.preventDefault;
   addLoginMenu();
   clearForm();
   localStorage.clear();
 });
+}*/
--- frontend/scripts/workout.js
+++ frontend/scripts/workout.js
@@ -9,13 +9,17 @@ const listWorkouts = async () => {
     const data = await response.json();
     console.log(data);
 
+    /**********************************************************/
+    // Får ej detta att funka
+
     // skapa DOM element
-    const div1 = document.createElement("div");
+    /*const div1 = document.createElement("div");
     const div2 = document.createElement("div");
-    const div3 = document.createElement("div");
+    const div3 = document.createElement("div");*/
 
     // Jag har ingen aning om vad jag gör här
-    const listExercises = async () => /*var ska await vara?*/ {
+    /*const listExercises = async () => //var ska await vara?
+    {
       data.forEach((exercises) => {
         const exercise = document.querySelector("#exercise");
         exercise.appendChild(div1);
@@ -25,9 +29,12 @@ const listWorkouts = async () => {
         div2.insertAdjacentHTML("beforeend", `<p>${exercises.reps}</p>`);
         div3.insertAdjacentHTML("beforeend", `<p>${exercises.weight}</p>`);
       });
-    };
-
+    };*/
+    /* ${listExercises()} // lägg i loopen nedan
     // Borde jag loopa igenom hela innan alla element är på rätt plats?
+    /**************************************************************/
+
+    // Nuvarande utseende är bara placeholder tills jag listat ut hur jag får koden ovan att funka
     data.forEach((exercises) => {
       workout.insertAdjacentHTML(
         "beforeend",
@@ -37,16 +44,24 @@ const listWorkouts = async () => {
             <p class="edit">E</p>
           </div>
           <div class="content" id="exercise"> 
-            ${listExercises()}
+            <p>${exercises.sets}</p>
+            <p>${exercises.reps}</p>
+            <p>${exercises.weight}</p>
+            <p class="edit">E</p>
           </div>
           <div class="add-set">Add set</div>
-        </div>
-        <div class="buttons">
-          <button>Add exercise</button>
-          <button>Logs</button>
         </div>`
       );
     });
+
+    const buttons = document.querySelector(".container");
+    buttons.insertAdjacentHTML(
+      "afterend",
+      ` <div class="buttons">
+          <button>Add exercise</button>
+          <button>Logs</button>
+        </div>`
+    );
   }
 };
 
