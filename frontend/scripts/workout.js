090664758d43a213d8e58c42b9ea67ff95aae76c
Behemoth39 <simme_jonsson@hotmail.com>
2023-04-27 11:48:22 +0200

    Working on getting the loop to work

--- frontend/scripts/workout.js
+++ frontend/scripts/workout.js
@@ -13,24 +13,24 @@ const listWorkouts = async () => {
     // I can´t get it to work this way
 
     // Create DOM elements
-    /*const div1 = document.createElement("div");
+    const div1 = document.createElement("div");
     const div2 = document.createElement("div");
-    const div3 = document.createElement("div");*/
+    const div3 = document.createElement("div");
 
     // No clue of how to get this to work
-    /*const listExercises = async () => //var ska await vara?
-    {
-      data.forEach((exercises) => {
-        const exercise = document.querySelector("#exercise");
-        exercise.appendChild(div1);
-        exercise.appendChild(div2);
-        exercise.appendChild(div3);
-        div1.insertAdjacentHTML("beforeend", `<p>${exercises.sets}</p>`);
-        div2.insertAdjacentHTML("beforeend", `<p>${exercises.reps}</p>`);
-        div3.insertAdjacentHTML("beforeend", `<p>${exercises.weight}</p>`);
-      });
-    };*/
-    /* ${listExercises()} // put in the loop below
+    const listExercises = async () =>
+      // await?
+      {
+        data.forEach((exercises) => {
+          const exercise = document.querySelector("#exercise");
+          exercise.appendChild(div1);
+          exercise.appendChild(div2);
+          exercise.appendChild(div3);
+          div1.insertAdjacentHTML("beforeend", `<p>${exercises.sets}</p>`);
+          div2.insertAdjacentHTML("beforeend", `<p>${exercises.reps}</p>`);
+          div3.insertAdjacentHTML("beforeend", `<p>${exercises.weight}</p>`);
+        });
+      };
     // Should I loop through the whole thing before all the elements are in their right places?
     /**************************************************************/
 
@@ -44,9 +44,7 @@ const listWorkouts = async () => {
             <p class="edit">E</p>
           </div>
           <div class="content" id="exercise"> 
-            <p>${exercises.sets}</p>
-            <p>${exercises.reps}</p>
-            <p>${exercises.weight}</p>
+            ${listExercises()}
             <p class="edit">E</p>
           </div>
           <div class="add-set">Add set</div>
@@ -78,3 +76,22 @@ const listWorkouts = async () => {
 };
 
 listWorkouts();
+
+/*data.forEach((exercises) => {
+      workout.insertAdjacentHTML(
+        "beforeend",
+        ` <div class="container">
+          <div class="head">
+            <h3>${exercises.exercise}</h3>
+            <p class="edit">E</p>
+          </div>
+          <div class="content" id="exercise"> 
+            <p>${exercises.sets}</p>
+            <p>${exercises.reps}</p>
+            <p>${exercises.weight}</p>
+            <p class="edit">E</p>
+          </div>
+          <div class="add-set">Add set</div>
+        </div>`
+      );
+    });*/
